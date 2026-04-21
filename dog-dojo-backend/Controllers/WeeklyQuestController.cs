3cc8bc38a4577438839ca22922bec2fc8147986f
Baguji <bernardotab@gmail.com>
2026-04-21 18:56:55 +0100

    Added file managing

---
+++ dog-dojo-backend/Controllers/WeeklyQuestController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace dog_dojo_backend.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class WeeklyQuestController : ControllerBase
+    {
+        private readonly string _currentQuestFile = "current_quest.json";
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            if (!System.IO.File.Exists(_currentQuestFile))
+                return NotFound("No quest selected yet.");
+
+            var json = await System.IO.File.ReadAllTextAsync(_currentQuestFile);
+
+            return Content(json, "application/json");
+        }
+    }
+}
---
+++ dog-dojo-backend/SideQuest.cs
@@ -0,0 +1,9 @@
+﻿namespace dog_dojo_backend
+{
+    public class SideQuest
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+    }
+}
